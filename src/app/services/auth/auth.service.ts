c839623b03b37e764a3f3fbfc5261540181439dd
Csordas Balazs <csordasbalu96@gmail.com>
2023-03-31 17:20:41 +0200

    cookie test

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ErrorHandlerService } from '../error-handler/error-handler.service';
 import { SnackbarService } from '../snackbar/snackbar.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
@@ -15,9 +16,10 @@ export class AuthService {
     private http: HttpClient,
     private errorHandler: ErrorHandlerService,
     private snackbar: SnackbarService,
-    private router: Router
+    private router: Router,
+    private cookieService: CookieService
   ) {}
-  accessToken: string | null = null;
+  accessToken: string | null = this.cookieService.get('jwt');
 
   sendLoginCredentials(loginForm: FormGroup) {
     this.http
@@ -30,6 +32,14 @@ export class AuthService {
       )
       .pipe(catchError((error) => this.errorHandler.handleError(error)))
       .subscribe((res) => {
+        this.cookieService.set(
+          'jwt',
+          res.accessToken,
+          1,
+          undefined,
+          undefined,
+          true
+        );
         this.accessToken = res.accessToken;
         this.router.navigateByUrl('');
         this.snackbar.success('Successfully logged in.');
